05fa38490c1ec77093e65c0c579f82386a76f7a4
Tajah Johnson <tajah93@hotmail.com>
2020-10-07 21:33:56 -0500

    Updated ORM

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,28 @@
 var connection = require("..config/connection.js");
 
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+  function objToSql(ob) {
+    var arr = [];
+    for (var key in ob) {
+      var value = ob[key];
+      if (Object.hasOwnProperty.call(ob, key)) {
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        arr.push(key + "=" + value);
+      }
+    }
+    return arr.toString();
+  }
+
 var orm = {
     selectAll: function(tableInput, callback) {
         var queryString = "SELECT * FROM " + tableInput + ";";
