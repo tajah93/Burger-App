7718b87904288e042903a4f03b3027bf0acff8e1
Tajah Johnson <tajah93@hotmail.com>
2020-10-07 19:50:58 -0500

    Boiler code for server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+ 
+var app = express();
+var PORT = process.env.PORT || 3306;
+
+app.use(express.static("public")); 
+
+app.engine("handlebars", exphbs({ defaultLayout : "main" }));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(PORT, function(){
+    console.log("Server is listening on: http://localhost:" + PORT)
+});
+
